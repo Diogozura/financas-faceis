bb1a5f78dad8626748d0e9dadf8fe48b167537c9
Diogo Zura <diogozura@gmail.com>
2023-01-16 12:26:28 -0300

    start project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import styled, { ThemeProvider } from 'styled-components'
+import GlobalStyle from "../styles";
+import {theme} from "../styles/theme";
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+        <GlobalStyle/>
+       <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
---
+++ styles/theme.tsx
@@ -0,0 +1,8 @@
+export const theme = {
+    colors: {
+        Amarelo: "#FDDC5C",
+        Vermelho: "#FA4224",
+        Azul: "#475F94",
+        AzulEscuro: "#232E48"
+    }
+}
