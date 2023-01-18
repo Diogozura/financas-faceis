7be3b1e2c80a3447f7560093693aa251c670434e
Diogo Zura <diogozura@gmail.com>
2023-01-18 19:47:52 -0300

    Update index.tsx

--- src/components/mesAno/index.tsx
+++ src/components/mesAno/index.tsx
@@ -10,16 +10,17 @@ export default function YearToMonthForm({ setMeses }:any) {
   const [month, setMonth] = useState<any>('');
 
   const handleYearChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const yearValue = event.target.value;
+    const yearValue:any = event.target.value;
     setYear(yearValue);
     setMonth(yearValue * 12);
     setMeses(yearValue * 12)
   };
 
   const handleMonthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const monthValue = event.target.value;
+    const monthValue:any = event.target.value;
     setMonth(monthValue);
     setMeses(monthValue)
+    
     setYear(monthValue / 12);
   };
 
