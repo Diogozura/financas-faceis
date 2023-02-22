eb7811c1d4399de9e4a4955ae62342021fc401e0
Diogo Zura <diogozura@gmail.com>
2023-02-22 17:15:53 -0300

    correção nas cores do footer

--- src/components/Base/Footer/index.tsx
+++ src/components/Base/Footer/index.tsx
@@ -1,6 +1,22 @@
 import { Box, List, ListItem, Typography } from "@mui/material";
+import styled from 'styled-components'
 import Link from "next/link";
 
+
+
+const Navigation = styled.a`
+color: #8D98BA;
+text-decoration: none;
+/* font-size: x-large; */
+cursor: pointer;
+@media only screen and (max-width: 550px){
+    margin: 5px;
+}
+&:hover{    
+
+    text-decoration: underline;
+    }
+`
 
 const footer = [
     {
@@ -70,26 +86,42 @@ const footer = [
 
 export default function Footer() {
     return (
-        <Box padding={3} mt={20} sx={{ backgroundColor: '#201E50' }}>
-              <Box width={'100%'} display={'flex'} flexWrap={'wrap'} justifyContent={'space-around'} >
-            {footer.map((value) => (
-                <>
-                    <List>
-                        <ListItem  sx={{ fontWeight: 700,fontSize:'1.5em', color:'#FFFF' }}>{value.categoria}</ListItem>
-                        <ListItem> <Link href={`${value.item1.link}`} style={{color:'#525B76', textDecoration:'none'}}>{value.item1.nome}</Link></ListItem>
-                        <ListItem> <Link href={`${value.item2.link}`} style={{color:'#525B76', textDecoration:'none'}}>{value.item2.nome}</Link></ListItem>
-                        <ListItem> <Link href={`${value.item3?.link}`} style={{color:'#525B76', textDecoration:'none'}}>{value.item3?.nome}</Link></ListItem>
-                        <ListItem> <Link href={`${value.item4?.link}`} style={{color:'#525B76', textDecoration:'none'}}>{value.item4?.nome}</Link></ListItem>
-                    </List>
-                   
-                </>
+        <Box padding={3} mt={0} sx={{ backgroundColor: '#201E50' }}>
+            <Box width={'100%'} display={'flex'} flexWrap={'wrap'} justifyContent={'space-around'} >
+                {footer.map((value) => (
+                    <>
+                        <List>
+                            <ListItem sx={{ fontWeight: 700, fontSize: '1.5em', color: '#FFFF' }}>{value.categoria}</ListItem>
+                            <ListItem>
+                                <Link href={`${value.item1.link}`} legacyBehavior>
+                                    <Navigation> {value.item1.nome}</Navigation>
+                                </Link>
+                            </ListItem>
+                            <ListItem>
+                                <Link href={`${value.item2.link}`} legacyBehavior>
+                                <Navigation> {value.item2.nome}</Navigation>
+                                </Link>
+                            </ListItem>
+                            <ListItem>
+                                <Link href={`${value.item3?.link}`} legacyBehavior >
+                                <Navigation> {value.item3?.nome}</Navigation>
+                                </Link>
+                            </ListItem>
+                            <ListItem>
+                                <Link href={`${value.item4?.link}`} legacyBehavior >
+                                <Navigation> {value.item4?.nome}</Navigation>
+                                </Link>
+                            </ListItem>
+                        </List>
 
-            ))}
-            
+                    </>
+
+                ))}
+
+            </Box>
+            <Typography color={'white'} variant={"h4"}>Tabelas funcionais</Typography>
         </Box>
-            <Typography color={'white'} variant={"h4" }>Tabelas funcionais</Typography>
-        </Box>
-      
-        
+
+
     )
 }
